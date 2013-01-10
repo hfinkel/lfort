9b3ab1a8948a6fc55521ce1ad67134e4c9f2ea2f
Hal Finkel <hfinkel@anl.gov>
2013-01-10 13:49:18 -0600

    fix lto driver test

--- test/Driver/lto.F90
+++ test/Driver/lto.F90
@@ -6,18 +6,18 @@
 
 ! and -emit-llvm doesn't alter pipeline (unfortunately?).
 ! RUN: %lfort -ccc-print-phases %s -emit-llvm 2> %t.log
-! RUN: grep '0: input, ".*lto.c", c' %t.log
-! RUN: grep '1: preprocessor, {0}, cpp-output' %t.log
+! RUN: grep '0: input, ".*lto.F90", f90-cpp-input' %t.log
+! RUN: grep '1: preprocessor, {0}, f90' %t.log
 ! RUN: grep '2: compiler, {1}, lto-bc' %t.log
 ! RUN: grep '3: linker, {2}, image' %t.log
 
 ! llvm-bc and llvm-ll outputs need to match regular suffixes
 ! (unfortunately).
 ! RUN: %lfort %s -emit-llvm -save-temps -### 2> %t.log
-! RUN: grep '"-o" ".*lto\.i" "-x" "c" ".*lto\.c"' %t.log
-! RUN: grep '"-o" ".*lto\.o" .*".*lto\.i"' %t.log
+! RUN: grep '"-o" ".*lto\.f90" "-x" "f90-cpp-input" ".*lto\.F90"' %t.log
+! RUN: grep '"-o" ".*lto\.o" .*".*lto\.f90"' %t.log
 ! RUN: grep '".*a.out" .*".*lto\.o"' %t.log
 
 ! RUN: %lfort %s -emit-llvm -S -### 2> %t.log
-! RUN: grep '"-o" ".*lto\.s" "-x" "c" ".*lto\.c"' %t.log
+! RUN: grep '"-o" ".*lto\.s" "-x" "f90-cpp-input" ".*lto\.F90"' %t.log
 
