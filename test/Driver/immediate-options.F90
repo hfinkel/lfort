d8885b98ad2aa9a2eb69bf277918c9443172badd
Hal Finkel <hfinkel@anl.gov>
2013-01-10 08:51:06 -0600

    test fix

--- test/Driver/immediate-options.F90
+++ test/Driver/immediate-options.F90
@@ -1,6 +1,6 @@
 ! RUN: %lfort --help | grep isystem
 ! RUN: %lfort --help | not grep ast-dump
-! RUN: %lfort --help | not grep ccc-cxx
-! RUN: %lfort --help-hidden | grep ccc-cxx
+! RUN: %lfort --help | not grep ccc-f77
+! RUN: %lfort --help-hidden | grep ccc-f77
 ! RUN: %lfort -dumpversion
 ! RUN: %lfort -print-search-dirs
