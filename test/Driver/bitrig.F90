9dc1a3df8ac8cb044a1dd364f293b3a45ea214e6
Hal Finkel <hfinkel@anl.gov>
2013-01-09 21:06:20 -0600

    fixup bitrig test

--- test/Driver/bitrig.F90
+++ test/Driver/bitrig.F90
@@ -3,27 +3,19 @@
 ! CHECK-LD-C: lfort{{.*}}" "-cc1" "-triple" "amd64-pc-bitrig"
 ! CHECK-LD-C: ld{{.*}}" {{.*}} "-lc" "-llfort_rt.amd64"
 
-! RUN: %lfortxx -no-canonical-prefixes -target amd64-pc-bitrig %s -### 2>&1 \
-! RUN:   | FileCheck --check-prefix=CHECK-LD-CXX %s
-! CHECK-LD-CXX: lfort{{.*}}" "-cc1" "-triple" "amd64-pc-bitrig" 
-! CHECK-LD-CXX: ld{{.*}}" {{.*}} "-lstdc++" "-lm" "-lc" "-llfort_rt.amd64"
-
-! RUN: %lfortxx -stdlib=libc++ -no-canonical-prefixes -target amd64-pc-bitrig %s -### 2>&1 \
-! RUN:   | FileCheck --check-prefix=CHECK-LD-CXX-STDLIB %s
-! CHECK-LD-CXX-STDLIB: lfort{{.*}}" "-cc1" "-triple" "amd64-pc-bitrig"
-! CHECK-LD-CXX-STDLIB: ld{{.*}}" {{.*}} "-lc++" "-lcxxrt" "-lgcc" "-lm" "-lc" "-llfort_rt.amd64"
+! FIXME: Add tests for finding the C++ runtime if necessary.
 
 ! RUN: %lfort -no-canonical-prefixes -target amd64-pc-bitrig -pthread %s -### 2>&1 \
 ! RUN:   | FileCheck --check-prefix=CHECK-PTHREAD %s
 ! CHECK-PTHREAD: lfort{{.*}}" "-cc1" "-triple" "amd64-pc-bitrig"
-! CHECK-PTHREAD: ld{{.*}}" {{.*}} "{{.*}}crtbegin.o" {{.*}}.o" "-lpthread" "-lc" "-llfort_rt.amd64" "{{.*}}crtend.o"
+! CHECK-PTHREAD: ld{{.*}}" {{.*}} "{{.*}}crtbegin.o" {{.*}}.o" "-lfortrt" "-lm" "-lpthread" "-lc" "-llfort_rt.amd64" "{{.*}}crtend.o"
 
 ! RUN: %lfort -no-canonical-prefixes -target amd64-pc-bitrig -pg -pthread %s -### 2>&1 \
 ! RUN:   | FileCheck --check-prefix=CHECK-PG-PTHREAD %s
 ! CHECK-PG-PTHREAD: lfort{{.*}}" "-cc1" "-triple" "amd64-pc-bitrig"
-! CHECK-PG-PTHREAD: ld{{.*}}" {{.*}} "{{.*}}crtbegin.o" {{.*}}.o" "-lpthread_p" "-lc_p" "-llfort_rt.amd64" "{{.*}}crtend.o"
+! CHECK-PG-PTHREAD: ld{{.*}}" {{.*}} "{{.*}}crtbegin.o" {{.*}}.o" "-lfortrt" "-lm_p" "-lpthread_p" "-lc_p" "-llfort_rt.amd64" "{{.*}}crtend.o"
 
 ! RUN: %lfort -no-canonical-prefixes -target amd64-pc-bitrig -shared -pg -pthread %s -### 2>&1 \
 ! RUN:   | FileCheck --check-prefix=CHECK-PG-PTHREAD-SHARED %s
 ! CHECK-PG-PTHREAD-SHARED: lfort{{.*}}" "-cc1" "-triple" "amd64-pc-bitrig"
-! CHECK-PG-PTHREAD-SHARED: ld{{.*}}" {{.*}} "{{.*}}crtbeginS.o" {{.*}}.o" "-lpthread" "-llfort_rt.amd64" "{{.*}}crtendS.o"
+! CHECK-PG-PTHREAD-SHARED: ld{{.*}}" {{.*}} "{{.*}}crtbeginS.o" {{.*}}.o" "-lfortrt" "-lm_p" "-lpthread" "-llfort_rt.amd64" "{{.*}}crtendS.o"
